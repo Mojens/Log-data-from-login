fae3b997b2c69253c1d0eae6810038aa3fdef047
Mojens <moha135h@stud.kea.dk>
2022-11-27 17:46:15 +0100

    Added Security for end points

--- src/main/java/com/example/logdatafrommember/logging/api/WhoReadTableController.java
+++ src/main/java/com/example/logdatafrommember/logging/api/WhoReadTableController.java
@@ -3,6 +3,7 @@ package com.example.logdatafrommember.logging.api;
 
 import com.example.logdatafrommember.logging.dto.hasAddedUser.WhoReadTableResponse;
 import com.example.logdatafrommember.logging.service.WhoReadTableService;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
